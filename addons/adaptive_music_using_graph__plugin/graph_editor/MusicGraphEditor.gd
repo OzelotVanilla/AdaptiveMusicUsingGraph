003d72bd2bac47001a096a052ba5f4e00197e3d3
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-06-11 15:25:18 +0900

    feature - Connecting node, and create slot on node

--- addons/adaptive_music_using_graph__plugin/graph_editor/MusicGraphEditor.gd
+++ addons/adaptive_music_using_graph__plugin/graph_editor/MusicGraphEditor.gd
@@ -17,6 +17,10 @@ signal finished_creating_new_file(path: StringName)
 ## The index of the tab, and the file itself will be emitted.
 signal close_selected_tab()
 
+## Emit when the node selecting status change.
+## With the information of a set recording current selecting nodes.
+signal node_select_status_changed(selected_nodes_set: Dictionary[MusicGraphNode, Variant])
+
 ## The operation mode of MusicGraphEditor.
 enum OperationMode
 {
@@ -35,10 +39,12 @@ var resource_store: AMUGResource
 ## Counter used for new node creation.
 var node_id_counter: int:
     get: return self.graph_store.node_id_counter
+    set(value): self.graph_store.node_id_counter = value
 
 ## Counter used for new edge creation.
 var edge_id_counter: int:
     get: return self.graph_store.edge_id_counter
+    set(value): self.graph_store.edge_id_counter = value
 
 ## Current operation mode of UI.
 var operation_mode: OperationMode:
@@ -46,21 +52,14 @@ var operation_mode: OperationMode:
         #print_debug(str("Set MusicGraphEditor's operation_mode to \"", OperationMode.find_key(m), "\"."))
         operation_mode = m
 
+## The set of current selected music graph node.
+var selected_nodes_set: Dictionary[MusicGraphNode, Variant] = {}
+
 ## Position of creating new node.
 var new_node_position: Vector2 = Vector2.ZERO
 
 @onready var shortcut_manager = MusicGraphEditorShortcutManager.new()
-const new_music_graph_dialog_scene = preload("res://addons/adaptive_music_using_graph__plugin/godot_ui/create_new_dialog/NewMusicGraphDialog.tscn")
-@onready var new_music_graph_dialog: NewMusicGraphDialog = self.__initNewMusicGraphDialog()
-func __initNewMusicGraphDialog():
-    var new_music_graph_dialog = self.new_music_graph_dialog_scene.instantiate()
-    new_music_graph_dialog.visible = false
-    new_music_graph_dialog.connect(
-        "created_new_file_from_editor",
-        func(path: StringName): self.finished_creating_new_file.emit(path)
-    )
-    self.add_child(new_music_graph_dialog)
-    return new_music_graph_dialog
+@onready var new_music_graph_dialog: NewMusicGraphDialog = $NewMusicGraphDialog
 
 # func _init() -> void:
 #     self.add_child.bind(
@@ -143,19 +142,83 @@ func addNode():
         self.new_node_position + Vector2(20, 40)
     )
 
+    # UI.
     # This `call_deferred` is required, to avoid error when removing the node.
     self.add_child.bind(MusicGraphNode.new(new_node)).call_deferred()
-    self.graph_store.addNode(new_node_id, new_node)
+
+    # Data.
+    self.graph_store.addNode(new_node)
 
     # Avoiding stacking together when called multiple times.
     self.new_node_position += Vector2(20, 40)
 
-func onSelectingNode(node: Node):
-    if node is GraphElement:
-        self.new_node_position = Vector2(node.offset_right, node.offset_top) + Vector2(20, 40)
+func getNode(name: StringName) -> MusicGraphNode:
+    return self.get_node(NodePath(name))
 
+## Should operate both UI (`self`) and storage (`graph_store: MusicGraph`).
+func removeNode(node: MusicGraphNode) -> void:
+    # First disconnect.
+    for edge in self.graph_store.getAdjacentEdgeOfNode(node.node_store.id):
+        # UI.
+        self.disconnect_node(
+            self.graph_store.getNode(edge.from_node).name, edge.from_slot,
+            self.graph_store.getNode(edge.to_node).name,   edge.to_slot
+        )
+        # Data: Do not need to remove here. Auto removed in `MusicGraph::removeNode`.
+
+    # Then delete.
+    # UI.
+    self.remove_child.bind(node).call_deferred()
+    # Data.
+    self.graph_store.removeNode(node.node_store)
+
+func onSelectingNode(node: GraphElement):
+    self.new_node_position = Vector2(node.offset_right, node.offset_top) + Vector2(20, 40)
+    if node is MusicGraphNode:
+        self.selected_nodes_set.set(node, null)
+
+    self.node_select_status_changed.emit(self.selected_nodes_set)
+
+func onDeselectingNode(node: Node):
+    if node is MusicGraphNode:
+        self.selected_nodes_set.erase(node)
+
+    self.node_select_status_changed.emit(self.selected_nodes_set)
+
+## Should operate both UI (`self`) and storage (`graph_store: MusicGraph`).
+func onConnectingNode(from_node__name: StringName, from_port: int, to_node__name: StringName, to_port: int):
+    # TODO: Check if can be connected.
+    # UI.
+    self.connect_node(from_node__name, from_port, to_node__name, to_port)
+
+    # Data.
+    var new_edge_id = self.edge_id_counter
+    self.edge_id_counter += 1
+    var from_node := self.getNode(from_node__name)
+    var to_node   := self.getNode(to_node__name)
+    var new_edge = MusicEdge.new(
+        new_edge_id, str("MusicEdge ", new_edge_id),
+        from_node.node_store.id, from_port,
+        to_node.node_store.id,   to_port
+    )
+
+## Remove selected node.
+## Should operate both UI (`self`) and storage (`graph_store: MusicGraph`).
+func onRemovingNode(node_names: Array[StringName]):
+    for n in self.selected_nodes_set.keys(): if n is MusicGraphNode:
+        self.removeNode(n)
+
+    # Since they are no longer exist, need to update (or, clean) the selected nodes' set.
+    self.selected_nodes_set.clear()
+    self.node_select_status_changed.emit(self.selected_nodes_set)
+
+## Will call new_music_graph_dialog.
 func onCreatingNewFileAction():
     self.new_music_graph_dialog.requestPopup("res://", true)
+
+## After created from new_music_graph_dialog.
+func onCreatedNewFile(path: StringName):
+    self.finished_creating_new_file.emit(path)
 
 func onSavingAction():
     if self.graph_store != null and self.resource_store != null:
@@ -164,4 +227,7 @@ func onSavingAction():
 func onClosingAction():
     # TODO: Ask for if whether save for unsaved file.
 
+    self.close_selected_tab.emit()
+
+func onOKToCloseSelected():
     self.close_selected_tab.emit()
--- addons/adaptive_music_using_graph__plugin/graph_editor/MusicGraphNode.gd
+++ addons/adaptive_music_using_graph__plugin/graph_editor/MusicGraphNode.gd
@@ -16,28 +16,71 @@ func loadFromStore():
     self.position_offset = self.node_store.ui_position
     self.name = self.node_store.name
     self.title = self.node_store.name
+    self.loadSlotInfoFromStore()
+
+## This load all slot from the storage node.[br]
+## Notice that the slot here is different from Godot's.
+## The slot contains in/out port and its panel for showing information.
+func loadSlotInfoFromStore():
+    # Clean existed content first.
+    self.clearAllSlots()
+
+    # Add from storage.
     for i in self.node_store.slot_info:
+        var slot_content: Control
+
         # If path-in slot (index 0 and only has left connection).
         if i.index == 0 and i.location == GraphNodeSlotInfo.SlotLocation.left:
-            var label = Label.new()
-            label.text = "In"
+            slot_content = Label.new()
+            slot_content.text = "Input"
         else:
-            var label_test = Label.new()
-            label_test.text = "Test"
-            self.add_child(label_test)
+            slot_content = Label.new()
+            slot_content.text = "Test"
 
-        self.set_slot(
-            i.index,
-            true if i.location == GraphNodeSlotInfo.SlotLocation.left else false,
-            i.connection_category,
-            i.colour,
-            true if i.location == GraphNodeSlotInfo.SlotLocation.right else false,
-            i.connection_category,
-            i.colour
-        )
+        self.add_child(slot_content)
+        self.setSlotBySlotInfo(i)
+
+func clearAllSlots() -> void:
+    # Clear port (Godot's slot).
+    self.clear_all_slots()
+
+    # Clear element
+    for node in self.get_children():
+        self.remove_child(node)
+        node.queue_free()
 
 func saveUIInfoToStore():
     node_store.ui_position = self.position_offset
+
+func setSlotBySlotInfo(info: GraphNodeSlotInfo):
+    self.set_slot(
+        info.index,
+        true if info.location == GraphNodeSlotInfo.SlotLocation.left or info.location == GraphNodeSlotInfo.SlotLocation.both
+                else false,
+        info.connection_category,
+        info.colour,
+        true if info.location == GraphNodeSlotInfo.SlotLocation.right or info.location == GraphNodeSlotInfo.SlotLocation.both
+                else false,
+        info.connection_category,
+        info.colour
+    )
+
+## Should operate both UI (`self`) and Data (`node_store: MusicNode`).
+func addInOutSlot() -> void:
+    # Data.
+    self.node_store.addSlot(GraphNodeSlotInfo.SlotLocation.both)
+
+    # UI.
+    self.loadSlotInfoFromStore()
+
+## Should operate both UI (`self`) and Data (`node_store: MusicNode`).
+func addOutSlot() -> void:
+    # Data.
+    self.node_store.addSlot(GraphNodeSlotInfo.SlotLocation.right)
+
+    # UI.
+    self.loadSlotInfoFromStore()
+
 
 func _to_string() -> String:
     return str(
