58ef9769bc3fd85b099ba2fbcacfa47f3345cd43
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-06-27 16:21:36 +0900

    struct - Runtime, bridging, and sound managing class

---
+++ addons/adaptive_music_using_graph__plugin/runtime/AMUGRuntime.gd
@@ -0,0 +1,9 @@
+# Runtime class. DO NOT put `@tool` here.
+class_name AMUGRuntime
+extends Node
+## Runtime logic and API.
+##
+## Registered as [code]amug[/code] by default to global scope (singleton).
+## Responsibility:[br]
+## * Receiving game param, provide play status.[br]
+# End of class document.
---
+++ addons/adaptive_music_using_graph__plugin/runtime/sound/SoundPlayManager.gd
@@ -0,0 +1,7 @@
+@tool
+class_name SoundPlayManager
+extends Node
+## Manage how sound should be played.
+##
+## Accept music graph and game param, and decide the play path.
+# End of class document.
